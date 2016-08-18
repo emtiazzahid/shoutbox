d68f57f2888fe2672d8fd097528058291e598ca3
emtiazzahid <emtiazzahid@gmail.com>
2016-08-18 20:34:34 +0600

    bootstrap installed

--- index.php
+++ index.php
@@ -4,9 +4,10 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>HOME</title>
-	<link rel="stylesheet" href="">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
 <body>
 	
+	<script src="js/bootstrap.min.js"></script>
 </body>
 </html>
