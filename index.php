90557280cb5399e055e245eaa10a7dde8206ca9b
emtiazzahid <emtiazzahid@gmail.com>
2016-08-18 21:23:56 +0600

    a simple layout created for shoutbox

--- index.php
+++ index.php
@@ -3,11 +3,69 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>HOME</title>
 	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/main.css">
 </head>
 <body>
-	
+
+<!-- comment -->
+	<div class="container">
+		<div class="jumbotron">
+			<div class="panel panel-default">
+			  <div class="panel-body">
+			    BASIC SHOUTBOX WITH PHP
+			  </div>
+				
+				<div class="box1">
+					<ul class="u">
+						<li>9:15 PM: <b>Zahid</b> Hi I am zahid</li>
+						<li>9:15 PM: <b>Zahid</b> Hi I am zahid</li>
+						<li>9:15 PM: <b>Zahid</b> Hi I am zahid</li>
+						<li>9:15 PM: <b>Zahid</b> Hi I am zahid</li>
+						<li>9:15 PM: <b>Zahid</b> Hi I am zahid</li>
+						<li>9:15 PM: <b>Zahid</b> Hi I am zahid</li>
+					</ul>
+				</div>
+
+				<div class="box2">
+					
+					<form class="form-horizontal">
+					  <div class="form-group">
+					    <label for="name" class="col-sm-2 control-label">Name</label>
+					    <div class="col-sm-10">
+					      <input type="text" class="form-control" id="name" placeholder="Your Name">
+					    </div>
+					  </div>
+					  <div class="form-group">
+					    <label for="sms" class="col-sm-2 control-label">SMS</label>
+					    <div class="col-sm-10">
+					      <input type="text" class="form-control" id="sms" placeholder="Your Massage">
+					    </div>
+					  </div>
+					  <div class="form-group">
+					    <div class="col-sm-offset-2 col-sm-10">
+					      <button type="submit" class="btn btn-default">Submit</button>
+					    </div>
+					  </div>
+					</form>
+									
+				</div>
+
+
+
+
+
+
+
+
+			  <div class="panel-footer">Source :: </div>
+			</div>
+		</div>	  
+	</div>
+
+
 	<script src="js/bootstrap.min.js"></script>
 </body>
 </html>
